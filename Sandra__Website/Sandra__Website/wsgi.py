5d77a147b30cd2275fd5a113a516b3634368ebe3
toilagatau234 <toilagatau234@gmail.com>
2025-09-10 19:07:42 +0700

    start project

---
+++ Sandra__Website/Sandra__Website/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Sandra__Website project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sandra__Website.settings')
+
+application = get_wsgi_application()
